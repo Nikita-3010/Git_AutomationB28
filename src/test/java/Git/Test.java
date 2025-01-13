8cad6d463ea28c3c9ee5c33340efab575163ad9e
Nikita-3010 <nikitagangurde2001@gmail.com>
2025-01-13 13:49:14 +0530

    TC001 Automated

--- src/test/java/Git/Test.java
+++ src/test/java/Git/Test.java
@@ -3,7 +3,9 @@ package Git;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("hello java");
+		System.out.println("hello nikita");
 
 	}
 
